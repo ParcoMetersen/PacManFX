2173d92be24bcc901f2f96e74277020a638d3abc
MattLV21 <Mattias.lange00@gmail.com>
2023-05-02 21:27:10 +0200

    code

---
+++ src/App.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    public static char dia = ' ';
+    public static Rectangle player = new Rectangle(150, 100, 50, 50);
+    public Rectangle ghost = new Rectangle(0, 150, 25, 25);
+
+    public static ArrayList<Rectangle> ghosts = new ArrayList<>();
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("you clicked me!");
+            }
+        });
+
+        
+        
+        Group root = new Group();
+        root.getChildren().add(btn);
+        
+        
+        root.getChildren().add(player);
+        ghosts.add(ghost);
+        root.getChildren().add(ghost);
+        
+
+        /*
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Scene scene = new Scene(root);
+        */
+
+        AnimationTimer loop = new TimerMethod();
+        loop.start();
+        
+        Scene scene = new Scene(root, 300, 250);
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+            if(key.getCode() == KeyCode.W) {
+                dia = 'W';
+            } else if(key.getCode() == KeyCode.S) {
+                dia = 'S';
+            } else if(key.getCode() == KeyCode.A) {
+                dia = 'A';
+            } else if(key.getCode() == KeyCode.D) {
+                dia = 'D';
+            }
+            
+            loop.start();
+        });
+
+        
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/TimerMethod.java
@@ -0,0 +1,48 @@
+import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+class TimerMethod extends AnimationTimer {
+    @Override
+    public void handle(long now) {
+        Rectangle ghostHit = collidtion();
+        if(ghostHit != null) {
+            App.player.setFill(Color.GREEN);
+            // game over
+            stop();
+        }
+        edgeCollidtion();
+
+
+
+        if(App.dia == 'W') {
+            App.player.setY(App.player.getY()-1);
+        } else if(App.dia == 'S') {
+            App.player.setY(App.player.getY()+1);
+        } else if(App.dia == 'A') {
+            App.player.setX(App.player.getX()-1);
+        } else if(App.dia == 'D') {
+            App.player.setX(App.player.getX()+1);
+        }
+    }
+
+    private Rectangle collidtion() {
+        for(int i = 0; i < App.ghosts.size(); i++) {
+            Rectangle g = App.ghosts.get(i);
+            if(App.player.getBoundsInParent().intersects(g.getBoundsInParent())) {
+                 return g;
+            }
+        }
+        return null;
+    }
+
+    private void edgeCollidtion() {
+        if(App.player.getY() + App.player.getHeight() > 250) {
+            App.player.setY(250-App.player.getHeight());
+            stop();
+        } else if(App.player.getY() < 0) {
+            App.player.setY(0);
+            stop();
+        }
+    }
+}
